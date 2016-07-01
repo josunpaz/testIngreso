6853bae19c2d13cd5b88946926651369688a0de0
josunpaz <josueds@hotmail.com.ar>
2016-07-01 20:03:54 -0300

    Este es el primer commit que hago

---
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var nom = document.getElementById('elNombre').value;
+	var edad = document.getElementById('laEdad').value;
+	alert("Su nombre es: "+nom+" y tiene "+edad+" años");
+	
+
+
+
+}
+
